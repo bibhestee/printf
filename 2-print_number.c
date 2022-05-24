cec2fed4e0f34307e7cab4ab84e66b0c5c048874
Thomas Feranmi <ttoluwaleke@gmail.com>
2022-05-24 04:40:37 -0700

    2-pr

--- 2-print_number.c
+++ 2-print_number.c
@@ -1,31 +1,31 @@
 #include "main.h"
-
-/**
- * print_number - print out a number in base 10
- * @num: pass the number as arg to the func
- * _printf - print out the character
- *
- * Return: returns the number of character printed
- */
-
-int print_number(int num)
-{
-	int i = 0;
-
-	if (num < 0)
-	{
-		_putchar('-');
-		i++;
-		num = -num;
-	}
-
-	if (num > 9)
-	{
-		print_number(num / 10);
-	}
-	_putchar('0' + (num % 10));
-	i++;
-
-	return (i);
-
-}
+  2
+  3 /**
+  4  * print_number - print out a number in base 10
+  5  * @num: pass the number as arg to the func
+  6  * _printf - print out the character
+  7  *
+  8  * Return: returns the number of character printed
+  9  */
+ 10
+ 11 int print_number(int num)
+ 12 {
+ 13         int i = 0;
+ 14
+ 15         if (num < 0)
+ 16         {
+ 17                 _putchar('-');
+ 18                 i++;
+ 19                 num = -num;
+ 20         }
+ 21
+ 22         if (num > 9)
+ 23         {
+ 24                 print_number(num / 10);
+ 25         }
+ 26         _putchar('0' + (num % 10));
+ 27         i++;
+ 28
+ 29         return (i);
+ 30
+ 31 }
